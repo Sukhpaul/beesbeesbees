0aba862a6c5527d9ce06c0efdd6b98b4187da207
Sukhpaul Nijjar <sukhpaul.nijjar@gmail.com>
2017-10-16 11:57:20 -0700

    Complete Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
-var Bee = function() {
+var Bee = function(food) {
+	Grub.call(this, food);
+	this.age = 5;
+	this.color = 'yellow';
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+
+Bee.prototype.job = 'keep on growing';
+
+Bee.prototype.constructor = Bee;
 
