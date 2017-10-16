5f62e680bc585afbd8b9e25a0b102f0346532bd4
Sukhpaul Nijjar <sukhpaul.nijjar@gmail.com>
2017-10-16 11:57:55 -0700

    Complete HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,18 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function(color) {
+	Bee.call(this, color);
+	this.age = 10;
+	this.job = 'make honey';
+	this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+HoneyMakerBee.prototype.makeHoney = function() {
+	this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+	this.honeyPot--;
+};
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
