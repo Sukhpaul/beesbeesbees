0fc9eaa76312682b235c8ba1c55d331d8f20ae48
Sukhpaul Nijjar <sukhpaul.nijjar@gmail.com>
2017-10-16 11:58:19 -0700

    Complete ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
-var ForagerBee = function() {
+var ForagerBee = function(color) {
+	Bee.call(this, color);
+	this.age = 10;
+	this.job = 'find pollen';
+	this.canFly = true;
+	this.treasureChest = [];
 };
+
+ForagerBee.prototype = Object.create(HoneyMakerBee.prototype);
+
+ForagerBee.prototype.forage = function(treasure) {
+	this.treasureChest.push(treasure);
+};
+
+ForagerBee.prototype.constructor = ForagerBee;
 
