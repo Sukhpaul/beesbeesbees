c05d86efbf66d855ec7210afe807d9ed67017359
Sukhpaul Nijjar <sukhpaul.nijjar@gmail.com>
2017-10-16 11:58:59 -0700

    Complete RetiredForager

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,21 @@
-var RetiredForagerBee = function() {
+var RetiredForagerBee = function(food, treasureChest) {
+	ForagerBee.call(this, treasureChest);
+	Grub.call(this, food);
+	this.color = 'grey';
+	this.age = 40;
+	this.job = 'gamble';
+	this.canFly = false;
 };
+
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+
+RetiredForagerBee.prototype.forage = function() {
+	return 'I am too old, let me play cards instead';
+};
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+	this.treasureChest.push(treasure);
+};
+
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
 
